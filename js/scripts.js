5b62b03bfc25c08266f52c5932ec3d55938219ba
Student <example@example.com>
2019-10-24 10:13:21 -0700

    add if/else scripts for showing voting reg/resources/enter age

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,15 @@ $(document).ready(function() {
   $("form#vote").submit(function(event) {
     event.preventDefault();
     var age = parseInt($("input#age").val());
+
+    if (age >= 18 && age<100) {
       $(".over18").show();
-    
+    } else if(age < 18){
+    $(".under18").show();
+  } else {
+    $(".enterNumber").show();
+     }
+
+
 });
 });
